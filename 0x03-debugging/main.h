c2be0bb5f50468b25a69370c81ac1d012cd13024
Brigitte275 <brigitteiannotta3@gmail.com>
2022-12-15 13:15:45 -0800

    debugging

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+void positive_or_negative(int i);
+int largets_numer(int a, int b, int c);
+void print_remainaing_days(int month, int day, int year);
+int_convert_day(int month, int day)
+
+#endif
