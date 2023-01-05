0635d0dcdf9b65f89a5d48282a260994873bf843
Brigitte275 <brigitteiannotta3@gmail.com>
2023-01-05 14:17:20 -0800

    function that prints a string, followed by a new line.

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,13 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+/**
+ * File: holberton.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x07-recursion directory.
+ */
+
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
